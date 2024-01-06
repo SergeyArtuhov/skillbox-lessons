db5036369a16612a4c19e131323b4d4aa701169a
jokerse <artuhovse@yandex.ru>
2024-01-06 17:02:19 +0300

    06.11.2024 17:01

--- Django/Django 4/lesson_03/mysite/shopapp/views.py
+++ Django/Django 4/lesson_03/mysite/shopapp/views.py
@@ -1,6 +1,7 @@
-from timeit import default_timer
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.models import Group
+from timeit import default_timer
 
 
 def shop_index(request: HttpRequest):
@@ -9,8 +10,14 @@ def shop_index(request: HttpRequest):
         ('Desktop', 1999),
         ('Iphone', 2999),
     ]
+
     context = {
         'time_running': default_timer(),
-        'products': products
     }
     return render(request, 'shopapp/shop-index.html', context=context)
+
+def groups_list(request: HttpRequest):
+    context = {
+        'groups': Group.objects.all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)
