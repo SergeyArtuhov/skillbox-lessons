26f13918fb37a8e9ae9fe2ded12dc01d8d83181e
Sergey Artuhov <artuhovse@yandex.ru>
2024-01-18 00:52:20 +0300

    lesson 3.3 Django

--- Django/Django 4/lesson_03/mysite/shopapp/views.py
+++ Django/Django 4/lesson_03/mysite/shopapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.models import Group
 from timeit import default_timer
+from .models import Product
 
 
 def shop_index(request: HttpRequest):
@@ -16,8 +17,16 @@ def shop_index(request: HttpRequest):
     }
     return render(request, 'shopapp/shop-index.html', context=context)
 
+
 def groups_list(request: HttpRequest):
     context = {
         'groups': Group.objects.all(),
     }
     return render(request, 'shopapp/groups-list.html', context=context)
+
+
+def products_list(request: HttpRequest):
+    context = {
+        'products_list': Product.objects.all()
+    }
+    return render(request, 'shopapp/products_list.html', context=context)
