a57e364be04d02dc2582c5e26a436b824f77cc51
Sergey Artuhov <artuhovse@yandex.ru>
2024-01-21 01:08:38 +0300

    lesson 3.4 Django

--- Django/Django 4/lesson_03/mysite/shopapp/views.py
+++ Django/Django 4/lesson_03/mysite/shopapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.models import Group
 from timeit import default_timer
-from .models import Product
+from .models import Product, Order
 
 
 def shop_index(request: HttpRequest):
@@ -20,7 +20,7 @@ def shop_index(request: HttpRequest):
 
 def groups_list(request: HttpRequest):
     context = {
-        'groups': Group.objects.all(),
+        'groups': Group.objects.prefetch_related('permissions').all(),
     }
     return render(request, 'shopapp/groups-list.html', context=context)
 
@@ -30,3 +30,9 @@ def products_list(request: HttpRequest):
         'products_list': Product.objects.all()
     }
     return render(request, 'shopapp/products_list.html', context=context)
+
+def orders_list(request: HttpRequest):
+    context = {
+        'orders': Order.objects.select_related('user').prefetch_related('products').all(),
+    } # это значит что необходимо загрузить все объекты Order вместе с пользователями и к ним еще будут присоеденины продукты
+    return render(request, 'shopapp/orders-list.html', context=context)
