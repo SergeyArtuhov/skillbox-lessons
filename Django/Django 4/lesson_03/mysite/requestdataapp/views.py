9c5cc3382d46d34b34c954d002ed5f655aaf3453
Sergey Artuhov <artuhovse@yandex.ru>
2024-04-01 00:27:25 +0300

    lesson 6 Django Forms and Validators

--- Django/Django 4/lesson_03/mysite/requestdataapp/views.py
+++ Django/Django 4/lesson_03/mysite/requestdataapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.core.files.storage import FileSystemStorage
+from .forms import UserBioForm, UploadFileForm
 
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
@@ -16,13 +17,23 @@ def process_get_view(request: HttpRequest) -> HttpResponse:
 
 
 def user_form(request: HttpRequest) -> HttpResponse:
-    return render(request, 'requestdataapp/user-bio-form.html')
+    context = {
+        "form": UserBioForm()
+    }
+    return render(request, 'requestdataapp/user-bio-form.html', context=context)
 
 
 def handle_file_upload(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST' and request.FILES.get('myfile'):
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        print('saved file', filename)
-    return render(request, 'requestdataapp/file-upload.html')
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            myfile = form.cleaned_data["file"]
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            print('saved file', filename)
+    else:
+        form = UploadFileForm()
+    context = {
+        "form": form
+    }
+    return render(request, 'requestdataapp/file-upload.html', context=context)
--- Django/Django 4/lesson_03/mysite/shopapp/views.py
+++ Django/Django 4/lesson_03/mysite/shopapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.models import Group
 from timeit import default_timer
 from .models import Product, Order
+from .forms import ProductForm
 
 
 def shop_index(request: HttpRequest):
@@ -30,6 +31,26 @@ def products_list(request: HttpRequest):
         'products_list': Product.objects.all()
     }
     return render(request, 'shopapp/products_list.html', context=context)
+
+
+def product_create(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":  # проверяем какой метод
+        form = ProductForm(request.POST)  # получаем заполненную форму
+        if form.is_valid():  # проверяем валидность формы
+            # name = form.cleaned_data['name'] Clean_data это словарь
+            # price = form.cleaned_data['price']
+            # Product.objects.create(name, price) Можно и так передавать, но это долго
+            # Product.objects.create(**form.cleaned_data)  # распаковываем словарь и передаем в базу данных всю форму
+            form.save()
+            url = reverse('shopapp:products_list')  # генерируем ссылку для перехода
+            return redirect(url)  # открываем ссылку
+    else:  # это если будет Гет запрос
+        form = ProductForm()
+    context = {
+        "form": form
+    }
+    return render(request, 'shopapp/create-product.html', context=context)
+
 
 def orders_list(request: HttpRequest):
     context = {
