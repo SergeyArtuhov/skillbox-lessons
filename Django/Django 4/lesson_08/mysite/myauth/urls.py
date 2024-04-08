1231564b977ec9c4d1bc825ed2ed1ad5d5821bfc
jokerse <artuhovse@yandex.ru>
2024-04-08 17:42:14 +0300

    Lesson 09 Django

--- Django/Django 4/lesson_08/mysite/myauth/urls.py
+++ Django/Django 4/lesson_08/mysite/myauth/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from .views import login_view
+from .views import get_cookie_view, set_cookie_view, set_session_view, get_session_view, MylogoutView
+from django.contrib.auth.views import LoginView
 
 app_name = "myauth"
 
 urlpatterns = [
-    path("login/", login_view, name="login")
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="myauth/login.html",  # имя шаблона, если нет то будет искать registration/login.html
+            redirect_authenticated_user=True,  # редирект для уже залогиненых пользоваетлей, в settings, без этого ищет account/profile
+        ),
+        name="login"),
+    # path("logout/", logout_view, name="logout"),
+    path("logout/", MylogoutView.as_view(), name="logout"),
+    path("cookie/get/", get_cookie_view, name="cookie-get"),
+    path("cookie/set/", set_cookie_view, name="cookie-set"),
+    path("session/get", get_session_view, name="session-get"),
+    path("session/set", set_session_view, name="session-set"),
 ]
